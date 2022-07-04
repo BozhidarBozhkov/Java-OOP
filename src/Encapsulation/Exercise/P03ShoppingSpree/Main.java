143aef7f6fdefa97567287477d2f98f96c10adeb
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-07-04 21:25:38 +0300

    Problem 3 -> Shopping Spree

---
+++ src/Encapsulation/Exercise/P03ShoppingSpree/Main.java
@@ -0,0 +1,12 @@
+package Encapsulation.Exercise.P03ShoppingSpree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+}
