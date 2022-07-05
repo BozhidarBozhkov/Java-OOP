ececea50dca4b84f241abe07a7a3247e0e41202e
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-07-05 20:50:49 +0300

    initial commit

---
+++ src/Inheritance/Lab/RandomArrayList/Main.java
@@ -0,0 +1,14 @@
+package Inheritance.Lab.RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RandomArrayList list = new RandomArrayList();
+
+        list.add(13);
+        list.add(42);
+        list.add(63);
+        list.add(51);
+    }
+
+}
