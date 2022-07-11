db59f276df43e4d7527a8db0c9f6c5f9c26749c5
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-07-11 22:05:31 +0300

    initial commit

---
+++ src/InterfacesAndAbstraction/Lab/SayHelloExtended/BasePerson.java
@@ -0,0 +1,16 @@
+package InterfacesAndAbstraction.Lab.SayHelloExtended;
+
+public abstract class BasePerson implements Person{
+
+    private String name;
+
+    protected BasePerson(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+}
