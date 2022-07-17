e62c3e001641fd90ea3a847c504a077c8a659625
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-07-17 16:28:07 +0300

    Problem 2 -> Vehicles Extension

---
+++ src/Polymorphism/Exercise/VehiclesExtension/Bus.java
@@ -0,0 +1,12 @@
+package Polymorphism.Exercise.VehiclesExtension;
+
+public class Bus extends Vehicle {
+
+    public final static double ADDITIONAL_FULL_BUS_CONSUMPTION = 1.4;
+
+
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+    }
+
+}
